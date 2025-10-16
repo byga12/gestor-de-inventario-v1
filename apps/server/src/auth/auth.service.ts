a0d3f7198be3c8ee860153676118f0ef4847d7c7
Gaby Shima <byga>
2025-10-16 16:58:53 -0300

    Change import from absolute to relative

--- apps/server/src/auth/auth.service.ts
+++ apps/server/src/auth/auth.service.ts
@@ -1,8 +1,8 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { UserService } from 'src/user/user.service';
 import { compare } from 'bcrypt';
 import { JwtPayload, SignInDto } from '@by/types';
+import { UserService } from '../user/user.service';
 
 @Injectable()
 export class AuthService {
--- apps/server/src/dashboard/dashboard.service.ts
+++ apps/server/src/dashboard/dashboard.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ProductService } from 'src/product/product.service';
-import { UserService } from 'src/user/user.service';
+import { UserService } from '../user/user.service';
 
 @Injectable()
 export class DashboardService {
